bea3cfd4ba10fea752e345fb4ce7421f108cef8e
Kamil Maciejczyk <kamil.maciejczyk@oracle.com>
2025-12-07 04:39:12 +0100

    Style i kontrast

--- ZANT/frontend/src/app/app.component.ts
+++ ZANT/frontend/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { ChatComponent } from './components/chat/chat.component';
 import { EwypFormComponent } from './components/ewyp-form/ewyp-form.component';
 import { CommonModule } from '@angular/common';
+import { ContrastService } from './services/contrast.service';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +15,19 @@ import { CommonModule } from '@angular/common';
 export class AppComponent {
   title = 'ZANT Frontend';
   showEwypForm = false;
+  isHighContrast = false;
+
+  constructor(private contrastService: ContrastService) {
+    this.contrastService.highContrast$.subscribe(
+      highContrast => this.isHighContrast = highContrast
+    );
+  }
 
   toggleView(): void {
     this.showEwypForm = !this.showEwypForm;
+  }
+
+  toggleContrast(): void {
+    this.contrastService.toggleContrast();
   }
 }
